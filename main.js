76870b6ebe7ddb615e4899fdb3f2618271683d44
CharlesGitCode <66923809+CharlesGitCode@users.noreply.github.com>
2020-08-23 19:55:51 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-feather.replace()
+feather.replace();
+
+$('#SubmitButton').click(function(){
+   const firstName = $('#FirstName').val();
+    const lastName = $('#LastName').val();
+    const email = $('#E-Mail').val();
+    console.log(firstName + lastName + email);
+    
+    $('.confirmation').text("Congratulation! You are on our mailing list.  A confirmation e-mail has been sent to " + firstName + " " + lastName + " at " + email +"!!!");
+});
